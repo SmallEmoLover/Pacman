dd38cf7ea45cc3a419e7e514357dc643e2e222ad
SmallEmoLover <missingtext@yandex.ru>
2020-06-29 00:13:35 +0300

    Изменение типа возвращаемого значения

--- Object.h
+++ Object.h
@@ -11,8 +11,8 @@ class Object
     public:
         virtual int getX() = 0;
         virtual int getY() = 0;
-        virtual int setWidth() = 0;
-        virtual int setHeight() = 0;
+        virtual void setWidth() = 0;
+        virtual void setHeight() = 0;
     protected:
         //координаты
         int x, y;
