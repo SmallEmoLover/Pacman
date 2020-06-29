28d0b584f036c799bc088870f5d2f12da2158d6b
DaniDori <->
2020-06-29 22:19:45 +0300

    изменение функций движения

--- Player.cpp
+++ Player.cpp
@@ -2,13 +2,8 @@
 Player::Player() 
 {
 	_status = CLASSIC;
-	_direction = UP;
 	_step = 1;
 	_powerCoefficient = 2;
-}
-way Player::getDirection() 
-{
-	return _direction;
 }
 mode Player::getStatus()
 {
@@ -18,78 +13,32 @@ void Player::setStatus(mode status)
 {
 	_status = status;
 }
-void Player::setDirection(way direction)
-{
-	_direction = direction;
-}
-
-int Player::getHeight()
-{
-	return _height;
-}
-
-int Player::getWidth()
-{
-	return _width;
-}
-
+//необходимо дописать условия возможности движения
 int Player::moveLeft()
 {
-	//���� ��������
-	if (_x - _step/*�� ������*/ && _status == CLASSIC)
-	{
+	if (_status == CLASSIC)
 		_x -= _step;
-		return 1;
-	}
-	if (_x - _step/*�� ������*/ && _status == POWER)
-	{
+	else
 		_x -= _step * _powerCoefficient;
-		return 1;
-	}
-	return 0;
 }
 int Player::moveRight()
 {
-	//���� ��������
-	if (_x + _step/*�� ������*/ && _status == CLASSIC) 
-	{
+	if (_status == CLASSIC)
 		_x += _step;
-		return 1;
-	}
-	if (_x + _step/*�� ������*/ && _status == POWER) 
-	{
+	else
 		_x += _step * _powerCoefficient;
-		return 1;
-	}
-	return 0;
 }
 int Player::moveUp()
 {
-	//���� ��������
-	if (_y + _step/*�� ������*/ && _status == CLASSIC) 
-	{
+	if (_status == CLASSIC)
 		_y += _step;
-		return 1;
-	}
-	if (_y + _step/*�� ������*/ && _status == POWER) 
-	{
+	else
 		_y += _step * _powerCoefficient;
-		return 1;
-	}
-	return 0;
 }
 int Player::moveDown()
 {
-	//���� ��������
-	if (_y - _step/*�� ������*/ && _status == CLASSIC)
-	{
+	if (_status == CLASSIC)
 		_y -= _step;
-		return 1;
-	}
-	if (_y - _step/*�� ������*/ && _status == POWER) 
-	{
+	else
 		_y -= _step * _powerCoefficient;
-		return 1;
-	}
-	return 0;
 }
