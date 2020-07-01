cb5f6efffe2a12ae2cff236c98b5228e28beb1dd
SmallEmoLover <missingtext@yandex.ru>
2020-07-01 07:06:12 +0300

    Переписывание аргументов функций на Object, фикс багов

--- Util.cpp
+++ Util.cpp
@@ -10,44 +10,29 @@
 //Основано на разности Минковского с учётом того, что все хитбоксы - прямоугольники
 bool CollisionDetect(int x11, int x12, int y11, int y12, int x21, int x22, int y21, int y22)
 {
-	if (x11 - x21 > 0 && x11 - x22 > 0 && x12 - x21 > 0 && x12 - x22 > 0) 
+	if ((x11 - x21 >= 0) && (x11 - x22 >= 0) && (x12 - x21 >= 0) && (x12 - x22 >= 0)) 
+	return false;
+	if ((x11 - x21 <= 0) && (x11 - x22 <= 0) && (x12 - x21 <= 0) && (x12 - x22 <= 0))
 		return false;
-	if (x11 - x21 < 0 && x11 - x22 < 0 && x12 - x21 < 0 && x12 - x22 < 0)
+	if ((y11 - y21 >= 0) && (y11 - y22 >= 0) && (y12 - y21 >= 0) && (y12 - y22 >= 0))
 		return false;
-	if (y11 - y21 > 0 && y11 - y22 > 0 && y12 - y21 > 0 && y12 - y22 > 0)
-		return false;
-	if (y11 - y21 < 0 && y11 - y22 < 0 && y12 - y21 < 0 && y12 - y22 < 0)
+	if ((y11 - y21 <= 0) && (y11 - y22 <= 0) && (y12 - y21 <= 0) && (y12 - y22 <= 0))
 		return false;
 
 	return true;
 }
 
-bool CollisionDetect(Player *player, Cat *cat)
+bool CollisionDetect(Object *first, Object *second)
 {
-	int x11 = player->getX();
-	int x12 = x11 + player->getWidth();
-	int y11 = player->getY();
-	int y12 = y11 + player->getHeight();
+	int x11 = first->getX();
+	int x12 = x11 + first->getWidth();
+	int y11 = first->getY();
+	int y12 = y11 + first->getHeight();
 
-	int x21 = cat->getX();
-	int x22 = x21 + cat->getWidth();
-	int y21 = cat->getY();
-	int y22 = x21 + cat->getHeight();
-
-	return CollisionDetect(x11, x12, y11, y12, x21, x22, y21, y22);
-}
-
-bool CollisionDetect(Player *player, Rubbish *rubbish)
-{
-	int x11 = player->getX();
-	int x12 = x11 + player->getWidth();
-	int y11 = player->getY();
-	int y12 = y11 + player->getHeight();
-
-	int x21 = rubbish->getX();
-	int x22 = x21 + rubbish->getWidth();
-	int y21 = rubbish->getY();
-	int y22 = x21 + rubbish->getHeight();
+	int x21 = second->getX();
+	int x22 = x21 + second->getWidth();
+	int y21 = second->getY();
+	int y22 = y21 + second->getHeight();
 
 	return CollisionDetect(x11, x12, y11, y12, x21, x22, y21, y22);
 }
--- Util.h
+++ Util.h
@@ -6,6 +6,7 @@
 */
 
 #pragma once
+
 #define WallCount 52
 
 #include "Cat.h"
@@ -16,9 +17,8 @@
 class Cat;
 
 //Столкнулись ли объекты
+bool CollisionDetect(Object *first, Object *second);
 bool CollisionDetect(int x11, int x12, int y11, int y12, int x21, int x22, int y21, int y22);
-bool CollisionDetect(Player *player, Cat *cat);
-bool CollisionDetect(Player *player, Rubbish *rubbish);
 
 //Расставление стен
 void CreateWalls(Wall* walls[WallCount]);
